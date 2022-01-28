965443ee6519ebc88998e80f8d98b0ddc6ce8bc0
yurikgs <yurikgs@outlook.com>
2022-01-27 21:06:03 -0300

    implementação do get e post de tema

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
    id:0,
-  // token:'',
-  // nome:'',
-  // foto:''
-  foto:'https://i.imgur.com/jx01Ii7.jpg',
-  nome:'Yuri Silva',
-  token:'l'
+  token:'',
+  nome:'',
+  foto:''
+  // foto:'https://i.imgur.com/jx01Ii7.jpg',
+  // nome:'Yuri Silva',
+  // token:'l'
 };
