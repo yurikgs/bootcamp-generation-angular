a4c5e4127a2b6d10b88e5e19e750490dabb32a44
yurikgs <yurikgs@outlook.com>
2022-01-29 12:30:32 -0300

    erro - cadastrando nova postagem ao invés de atualizar

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -19,8 +19,21 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://bloggyme.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://bloggyme.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://bloggyme.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    console.log(postagem)
+    return this.http.put<Postagem>('https://bloggyme.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number) {
+    return this.http.delete(`https://bloggyme.herokuapp.com/postagens/${id}`, this.token)
   }
 
 }
